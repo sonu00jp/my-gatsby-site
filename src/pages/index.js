c7a32e310083f2eec2f4ca57b31381da4a457de5
Takaaki Shiota <sonu00jp@gmail.com>
2021-10-05 01:41:05 +0900

    Finished Gatsby Tutorial Part 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,15 @@
 import * as React from 'react';
-import Layout  from '../components/layout';
+import Layout from '../components/layout';
+import { StaticImage } from 'gatsby-plugin-image';
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage
+        src="../images/woman-3.jpeg"
+        alt="a beauty woman"
+      />
     </Layout>
   )
 }
